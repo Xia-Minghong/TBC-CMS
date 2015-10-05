72161283613a881413c586ec4cbeac074df0b85d
Zhou <zhou0235@e.ntu.edu.sg>
2015-10-05 15:59:57 +0800

    Communication models

---
+++ Communication/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from incident.models import Incident
+# Create your models here.
+class SocialMediaReport(models.Model):
+    incident = models.ForeignKey(Incident)
+    timestamp = models.DateTimeField('time published')
+    socialMediaText = models.TextField()

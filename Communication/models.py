75fb2128b3a1ae6cb36f00dfc64a99ede7524d15
Litucao <tucao@172.22.140.160>
2015-10-10 20:58:40 +0800

    test1

--- Communication/models.py
+++ Communication/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from incident.models import Incident
 # Create your models here.
+# commmet1
 class SocialMediaReport(models.Model):
     incident = models.ForeignKey(Incident)
     timestamp = models.DateTimeField('time published')

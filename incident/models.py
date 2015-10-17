1a1426587d7932a9b4ea21a17698f33cd5744936
weiyumou <weiyumou@yumous-macbook-pro.local>
2015-10-17 15:00:12 +0800

    minor amendments

--- incident/models.py
+++ incident/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from agency.models import Agency
+from datetime import datetime
 # Create your models here.
 
 class Incident(models.Model):
@@ -19,7 +20,7 @@ class Incident(models.Model):
     name = models.CharField(max_length = 50)
     status = models.CharField(max_length = 20, choices = inci_status, default = 'initiated')
     severity = models.IntegerField()
-    time = models.DateTimeField('time reported')
+    time = models.DateTimeField('time reported', default = datetime.now())
     location = models.CharField(max_length = 100)
     contact = models.IntegerField()
     type = models.CharField(max_length = 50, choices = inci_type)
@@ -36,10 +37,10 @@ class InciUpdate(models.Model):
     is_approved = models.BooleanField(default = False)
     updated_severity = models.IntegerField()
     description = models.TextField()
-    time = models.DateTimeField('time updated')
+    time = models.DateTimeField('time updated', default = datetime.now())
     
 class Dispatch(models.Model):
     incident = models.ForeignKey(Incident)
     agency = models.ForeignKey(Agency)
     resource = models.TextField()
-    time = models.DateTimeField('time dispatched')
+    time = models.DateTimeField('time dispatched', default = datetime.now())

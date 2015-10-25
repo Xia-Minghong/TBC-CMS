65e3eddf01db0be5c3d5e8420a2a50d733502db6
Jiaxiang <fujiaxiang1995@gmail.com>
2015-10-25 21:24:45 +0800

    added in admin page

--- App/urls.py
+++ App/urls.py
@@ -33,4 +33,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include(router.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 ]
--- incident/admin.py
+++ incident/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Incident
 # Register your models here.
+
+class IncidentAdmin(admin.ModelAdmin):
+    model = Incident
+    list_display = ('name', 'status')
+
+admin.site.register(Incident)
